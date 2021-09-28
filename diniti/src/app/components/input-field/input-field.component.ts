a1b9fa5ce2880854ed310fde3d2b91b656383e2e
Louis D'Hont <xevrobelgium@gmail.com>
2021-09-28 14:02:15 +0200

    Added some components and login content

---
+++ diniti/src/app/components/input-field/input-field.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-input-field',
+  templateUrl: './input-field.component.html',
+  styleUrls: ['./input-field.component.scss'],
+})
+export class InputFieldComponent implements OnInit {
+
+  @Input() placeholder: string;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- diniti/src/app/screens/login/login.module.ts
+++ diniti/src/app/screens/login/login.module.ts
@@ -7,6 +7,8 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import {InputFieldComponent} from "../../components/input-field/input-field.component";
+import {ButtonComponent} from "../../components/button/button.component";
 
 @NgModule({
   imports: [
@@ -15,6 +17,6 @@ import { LoginPage } from './login.page';
     IonicModule,
     LoginPageRoutingModule
   ],
-  declarations: [LoginPage]
+  declarations: [LoginPage, InputFieldComponent, ButtonComponent]
 })
 export class LoginPageModule {}
