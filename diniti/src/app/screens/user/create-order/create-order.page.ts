f2b5d8f0fd2cd0223e0b672a1bc0956ed469491f
Louis D'Hont <xevrobelgium@gmail.com>
2021-10-08 15:39:39 +0200

    Added a Router Extension Service to get the previous route

--- diniti/src/app/screens/user/create-order/create-order.page.ts
+++ diniti/src/app/screens/user/create-order/create-order.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RouterExtensionService} from '../../../services/ui-services';
 
 @Component({
   selector: 'app-create-order',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateOrderPage implements OnInit {
 
-  constructor() { }
+  constructor(private routerExtensionService: RouterExtensionService) {
+  }
 
   ngOnInit() {
   }
 
+  getPreviousUrl() {
+    return this.routerExtensionService.getPreviousUrl();
+  }
 }
