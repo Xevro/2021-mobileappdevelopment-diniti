3d5b03229e7832b2877a23e009d2ac1cef72a320
Louis D'Hont <xevrobelgium@gmail.com>
2021-10-19 00:33:56 +0200

    Added get request caching in the interceptor service

--- diniti/src/app/services/core-services/intercept.service.ts
+++ diniti/src/app/services/core-services/intercept.service.ts
@@ -1,22 +1,39 @@
 import {Injectable} from '@angular/core';
-import {HttpRequest, HttpHandler, HttpEvent, HttpInterceptor} from '@angular/common/http';
+import {HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
-import {catchError} from 'rxjs/operators';
+import {catchError, filter, first, shareReplay} from 'rxjs/operators';
 import {AuthenticationService} from '../authentication-services';
 
 @Injectable()
 export class InterceptService implements HttpInterceptor {
+  public readonly store: Record<string, Observable<HttpEvent<any>>> = {};
+
   constructor(private authenticationService: AuthenticationService) {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (request.method !== 'GET') {
+      return this.handleRequest(request, next);
+    } else {
+      if (request.url.indexOf('users/me') > -1) {
+        return this.handleRequest(request, next);
+      }
+      const cachedObservable = this.store[request.urlWithParams] ||
+        (this.store[request.urlWithParams] = next.handle(request).pipe(
+          filter((res) => res instanceof HttpResponse),
+          shareReplay(1),
+        ));
+      return cachedObservable.pipe(first());
+    }
+  }
+
+  private handleRequest(request: HttpRequest<any>, next: HttpHandler) {
     return next.handle(request).pipe(catchError(err => {
       if (err.status === 401) {
         // auto logout if 401 response returned from api
         this.authenticationService.logOut();
         location.reload(true);
       }
-
       const error = err.error.message || err.statusText;
       return throwError(error);
     }));
