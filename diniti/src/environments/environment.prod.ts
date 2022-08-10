3aaacefcee9e9d84c861141468f2ac6d063d6af7
Louis D'Hont <xevrobelgium@gmail.com>
2022-08-10 16:28:16 +0200

    Delete environment.prod.ts

--- diniti/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  api: 'https://parseapi.back4app.com/parse',
-  applicationId: '9XkRYSMQJRPVQJV9rtnLlCnknymynJtODtU5u2I7',
-  restApiKey: 'g3L4WgGiaIShz4gWHSLTZKBgVId6t1iDy59gqrs1'
-};
